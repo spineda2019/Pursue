1ccf39ce1d70814b2cf78ed263d5c6f39584d630
spineda2019 <spineda.wpi.alum@gmail.com>
2024-06-13 13:21:07 -0400

    add functionality for --version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use argparse::{ArgumentParser, StoreTrue};
 
+const VERSION: &str = "0.0.1";
+
+fn print_version_info() {
+    println!("Trace version {}", VERSION);
+}
+
 fn main() {
     let mut logging: bool = false;
+    let mut print_version: bool = false;
 
     {
         let mut argument_parser: ArgumentParser = ArgumentParser::new();
@@ -13,7 +20,18 @@ fn main() {
             "Output logging of file locations",
         );
 
+        argument_parser.refer(&mut print_version).add_option(
+            &["-v", "--version"],
+            StoreTrue,
+            "Print version and license information",
+        );
+
         argument_parser.parse_args_or_exit();
+    }
+
+    if print_version {
+        print_version_info();
+        return;
     }
 
     println!("Logging: {}", logging);

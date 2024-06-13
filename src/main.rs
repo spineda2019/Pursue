550b6594a11732b50941063290ece47cfbd6eaf9
spineda2019 <spineda.wpi.alum@gmail.com>
2024-06-13 13:16:23 -0400

    add logging flag

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use argparse::ArgumentParser;
+use argparse::{ArgumentParser, StoreTrue};
 
 fn main() {
-    println!("Hello, world!");
+    let mut logging: bool = false;
+
     {
-        let argument_parser: ArgumentParser = ArgumentParser::new();
+        let mut argument_parser: ArgumentParser = ArgumentParser::new();
+        argument_parser.set_description("Check your codebase FAST");
+
+        argument_parser.refer(&mut logging).add_option(
+            &["-l", "--log"],
+            StoreTrue,
+            "Output logging of file locations",
+        );
+
+        argument_parser.parse_args_or_exit();
     }
+
+    println!("Logging: {}", logging);
 }

4a6909edecd9720fca9670031bdc63593c53a425
spineda2019 <spineda.wpi.alum@gmail.com>
2024-06-22 00:39:17 -0400

    embed comment information into enum

--- src/filetype.rs
+++ src/filetype.rs
@@ -1,10 +1,42 @@
-pub enum FileType {
-    C,
-    Cpp,
-    Python,
-    Rust,
-    Zig,
-    Javascript,
-    Typescript,
-    Makefile,
+pub enum FileType<'b> {
+    C {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Cpp {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Python {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Rust {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Zig {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Javascript {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Typescript {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
+    Makefile {
+        inline_comment_fomrat: Option<&'b str>,
+        multiline_comment_start_format: Option<&'b str>,
+        multiline_comment_end_format: Option<&'b str>,
+    },
 }

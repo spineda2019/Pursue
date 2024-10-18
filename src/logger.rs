e0d22745f2f8b0be8187b6f0458ac89ab1ef91c2
spineda2019 <spineda.wpi.alum@gmail.com>
2024-10-18 02:08:15 -0400

    improve perf for lower sized repos (~1 mil files)

--- src/logger.rs
+++ src/logger.rs
@@ -18,34 +18,22 @@ use std::{
     io::{BufRead, BufReader, ErrorKind},
     num::NonZero,
     path::{Path, PathBuf},
-    sync::{Arc, Mutex, RwLock},
+    sync::{Arc, Condvar, Mutex, RwLock},
     thread,
 };
 
 use crate::filetype::{destructure_filetype, stringify_filetype, FileType};
 
 pub struct Logger {
-    data: Arc<Mutex<VecDeque<PathBuf>>>,
-    finish_flag: Arc<RwLock<bool>>,
-    line_count: Arc<Mutex<usize>>,
-    keyword_table: Arc<Mutex<HashMap<Arc<str>, usize>>>,
-    filetype_table: Arc<Mutex<HashMap<Arc<str>, usize>>>,
+    data: Mutex<VecDeque<PathBuf>>,
+    finish_flag: RwLock<bool>,
+    line_count: Mutex<usize>,
+    keyword_table: Mutex<HashMap<Arc<str>, usize>>,
+    filetype_table: Mutex<HashMap<Arc<str>, usize>>,
+    data_condition: Condvar,
+    data_condition_lock: Mutex<()>,
     root_directory: PathBuf,
     verbose: bool,
-}
-
-impl Clone for Logger {
-    fn clone(&self) -> Self {
-        Self {
-            data: self.data.clone(),
-            finish_flag: self.finish_flag.clone(),
-            line_count: self.line_count.clone(),
-            keyword_table: self.keyword_table.clone(),
-            filetype_table: self.filetype_table.clone(),
-            root_directory: self.root_directory.clone(),
-            verbose: self.verbose,
-        }
-    }
 }
 
 impl<'a> Logger {
@@ -60,11 +48,13 @@ impl<'a> Logger {
         }
 
         Self {
-            data: Arc::new(Mutex::new(VecDeque::new())),
-            finish_flag: Arc::new(RwLock::new(false)),
-            line_count: Arc::new(Mutex::new(0)),
-            keyword_table: Arc::new(Mutex::new(comment_table)),
-            filetype_table: Arc::new(Mutex::new(HashMap::new())),
+            data: Mutex::new(VecDeque::new()),
+            finish_flag: RwLock::new(false),
+            line_count: Mutex::new(0),
+            keyword_table: Mutex::new(comment_table),
+            filetype_table: Mutex::new(HashMap::new()),
+            data_condition: Condvar::new(),
+            data_condition_lock: Mutex::new(()),
             root_directory: directory,
             verbose: verbose_printing,
         }
@@ -96,9 +86,10 @@ impl<'a> Logger {
     fn increment_keyword(&self, keyword: &str) {
         if let Some(value) = self.keyword_table.lock().unwrap().get_mut(keyword) {
             *value += 1;
-        } else {
-            self.keyword_table.lock().unwrap().insert(keyword.into(), 1);
+            return;
         }
+
+        self.keyword_table.lock().unwrap().insert(keyword.into(), 1);
     }
 
     fn increment_filetype_frequency(&self, filetype: &FileType) {
@@ -348,23 +339,24 @@ impl<'a> Logger {
         }
     }
 
+    unsafe fn unsafe_waiting_room(handle: LogWrapper) {
+        let LogWrapper(inner) = handle;
+        (*inner).waiting_room();
+    }
+
     fn waiting_room(&self) {
         loop {
-            let entry: Option<PathBuf>;
             {
-                entry = self.data.lock().unwrap().pop_front();
+                let guard = self.data_condition_lock.lock().unwrap();
+                let _unused = self.data_condition.wait(guard).unwrap();
             }
 
-            match entry {
-                None => {
-                    if *self.finish_flag.read().unwrap() {
-                        return;
-                    } else {
-                        continue;
-                    }
-                }
-                Some(found_file) => self.parse_file(&found_file),
-            };
+            if let Some(found_file) = self.data.lock().unwrap().pop_front() {
+                self.parse_file(&found_file);
+            } else if *self.finish_flag.read().unwrap() {
+                self.data_condition.notify_all();
+                return;
+            }
         }
     }
 
@@ -376,10 +368,12 @@ impl<'a> Logger {
                     self.populate_queue(&entry.path())?;
                 } else {
                     self.data.lock().unwrap().push_back(entry.path());
+                    self.data_condition.notify_one();
                 }
             }
         } else {
             self.data.lock().unwrap().push_back(root.to_path_buf());
+            self.data_condition.notify_one();
         }
 
         Ok(())
@@ -403,19 +397,36 @@ impl<'a> Logger {
             worker_count
         );
 
-        let mut workers: Vec<thread::JoinHandle<()>> = vec![];
+        let mut workers: Vec<thread::JoinHandle<()>> = Vec::with_capacity(worker_count.get());
 
-        for _ in 0..worker_count.get() {
-            let self_clone = self.clone();
-            workers.push(thread::spawn(move || {
-                self_clone.waiting_room();
-            }));
+        // keep one instance of object with internal locks
+        unsafe {
+            for _ in 0..worker_count.get() {
+                let handle: LogWrapper = LogWrapper(self as *mut Logger);
+                workers.push(thread::spawn(|| {
+                    Self::unsafe_waiting_room(handle);
+                }));
+            }
         }
 
         self.populate_queue(&self.root_directory)?;
 
+        loop {
+            let size = match self.data.try_lock() {
+                Ok(s) => s.len(),
+                _ => continue,
+            };
+
+            if size == 0 {
+                break;
+            } else {
+                self.data_condition.notify_all();
+            }
+        }
+
         {
             *self.finish_flag.write().unwrap() = true;
+            self.data_condition.notify_all();
         }
 
         for worker in workers {
@@ -427,3 +438,7 @@ impl<'a> Logger {
         Ok(())
     }
 }
+
+struct LogWrapper(*mut Logger);
+
+unsafe impl Send for LogWrapper {}
